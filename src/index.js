a17de68c0137d95c6f731b8ad7e568aca78563e1
Bruno <bruno.poc@gmail.com>
2019-06-15 11:06:47 -0300

    making a react version of the project with some fixs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './_styles/_css/index.css'
 import App from './_components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
